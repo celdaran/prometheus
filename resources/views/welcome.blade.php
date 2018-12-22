9a0319e29041d9e61e14f47664b33a216a12a68e
Charlie Hills <github@phizzy.com>
2018-12-22 10:14:17 -0600

    Add data to the prototype app
    * Created a Link model
    * Fake/seed data
    * Update view to loop through data for display
    * Update route to fetch data and pass to view
    
    This completes the first feasibility round. I'm going to try to
    install this on my bluehost account next.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,13 +84,22 @@
                     Laravel
                 </div>
 
+                <div class="subtitle m-b-md">
+                    with a database connection
+                </div>
+
                 <div class="links">
+                    @foreach ($links as $link)
+                        <a href="{{ $link->url }}">{{ $link->title }}</a>
+                    @endforeach
+                    <!--
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    -->
                 </div>
             </div>
         </div>
